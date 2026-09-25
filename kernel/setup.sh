5d9632d7f0b381d96ac7a6c3eefee3a0b5010858
Arden Vey <ardenvey66@gmail.com>
2026-09-25 07:52:48 +0700

    Update setup.sh

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -2,8 +2,8 @@
 set -eu
 
 GKI_ROOT=$(pwd)
-OWNER="KernelSU-Next"
-REPO="$OWNER"
+OWNER="KernelSU-Next-Multimanager"
+REPO="JBHPocong"
 
 display_usage() {
     echo "Usage: $0 [--cleanup | <commit-or-tag>]"
